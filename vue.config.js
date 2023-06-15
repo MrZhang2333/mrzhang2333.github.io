891f2cca3847ff41bf4defac79ab6096a149e6f5
CrazyZhang233 <1401562907@qq.com>
2023-06-15 10:48:06 +0800

    23.6.15 update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   transpileDependencies: true
 }
+const { defineConfig } = require('@vue/cli-service')
+	module.exports = defineConfig({
+	  transpileDependencies: true,
+	  //关闭eslint校验
+	  lintOnSave: false
+	})
+
+
